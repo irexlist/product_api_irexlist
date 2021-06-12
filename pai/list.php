051523347fa8b5e5934aaf8168a2cc79ac5d000d
IREXLIST <74267928+irexlist@users.noreply.github.com>
2021-06-12 16:14:59 +0430

    Add files via upload

---
+++ pai/list.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>product api irexlist</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<div class="text">shope name </div>
+	<span class="price"><?php require_once('../wp-load.php'); $product = wc_get_product( 1471 ); echo $product->get_price(); ?> LEDGER NANO  s </span></p>
+<!-- wc_get_product( 1471 ) شناسه محصول وردپرس میباشد  -->
+	
+</body>
+</html>
